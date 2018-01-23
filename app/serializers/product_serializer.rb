b7ce6ebd89c34ff31f7d86c8d43af969293c71b6
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-23 04:39:11 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -2,7 +2,7 @@ class ProductSerializer
   def self.serialize(product)
     serialized_post = '{'
     serialized_post += '"name": ' + product.name + ', '
-    serialized_post += '"price": "' + product.price + '", '
+    serialized_post += '"price": "' + product.price.to_s + '", '
     serialized_post += '"inventory": "' + post.inventory + '", '
     serialized_post += '"description": "' + post.description + '", '
     serialized_post += '}'
